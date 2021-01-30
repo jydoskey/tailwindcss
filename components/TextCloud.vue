35de64e14e29a5847778fd8ccf722fa04520b784
jide_omotola <jide@reach.africa>
2021-01-30 20:40:40 +0100

    created file for tag cloud

---
+++ components/TextCloud.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="textcloud">
+    <ul class="text-black-200 text-lg">
+      <li><a href="/tag/word1" data-weight="3">World1</a></li>
+      <li><a href="/tag/word2" data-weight="7">World2</a></li>
+      <li><a href="/tag/word3" data-weight="4">World3</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
